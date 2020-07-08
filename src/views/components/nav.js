c7e80e02514546c0968ff2ba8e0214ec59cfbde2
Omar Ibrahim <oielbanna@gmail.com>
2020-07-07 23:25:10 -0400

    crazy nav markdown done

--- src/views/components/nav.js
+++ src/views/components/nav.js
@@ -6,6 +6,30 @@ import { motion, useViewportScroll, useTransform, useSpring } from "framer-motio
 import LanguageSelector from "./LanguageSelector";
 // import Pattern from "./nav-art";
 
+export function Links(props){
+    const data = [{
+        id: "linkedin",
+        label: "LN",
+        link: "https://www.linkedin.com/in/oielbanna"
+    },{
+        id: "github",
+        label: "GT",
+        link: "https://github.com/oielbanna"
+    },{
+        id: "twitter",
+        label: "TW",
+        link: "https://twitter.com/notelbanna"
+    }]
+
+    return (
+        <ul {...props}>
+            {data.map(d => {
+                return <li><A href={d.link} key={d.id} target="_blank">{d.label}</A></li>
+            })}
+        </ul>
+    )
+}
+
 export const Navigation = () => {
     const [isHovered, setHovered] = useState(false)
     const [isClicked, setClicked] = useState(true)
@@ -35,37 +59,56 @@ export const Navigation = () => {
             </A>
 
             <motion.div className="nav-container" initial={false} animate={{ translateX: isClicked ? "100%" : isHovered ? "5%" : "0%" }} transition={{ duration: 0.1, type: "spring", stiffness: 20 }}>
-                <h1 dangerouslySetInnerHTML={{ __html: t("nav-lost?") }} />
-                <div className="border" />
-                <ul className="nav-container_menu">
-                    <li>
-                        <A href="#">
-                            {t('home')}
-                        </A>
-                    </li>
-                    <li>
-                        <A href="#about">
-                            {t('about')}
-                        </A>
-                    </li>
-                    <li>
-                        <A href="#experience">
-                            {t('experience')}
-                        </A>
-                    </li>
-                    <li>
-                        <A href="#projects">
-                            {t('projects')}
-                        </A>
-                    </li>
-                    <li>
-                        <A href="#contact">
-                            {t('contact')}
-                        </A>
-                    </li>
-                </ul>
+                <div className="nav-container-left" >
 
-                <div className="graphic_area">
+                    <div className="text">
+                        <h1 dangerouslySetInnerHTML={{ __html: t("nav-lost?") }} />
+                        <div className="border" />
+                        <ul className="menu">
+                            <li>
+                                <A href="#">
+                                    {t('home')}
+                                </A>
+                            </li>
+                            <li>
+                                <A href="#about">
+                                    {t('about')}
+                                </A>
+                            </li>
+                            <li>
+                                <A href="#experience">
+                                    {t('experience')}
+                                </A>
+                            </li>
+                            <li>
+                                <A href="#projects">
+                                    {t('projects')}
+                                </A>
+                            </li>
+                            <li>
+                                <A href="#contact">
+                                    {t('contact')}
+                                </A>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className="contact">
+
+                        <span class='the-arrow -right'>
+                            <span class='shaft'></span>
+                        </span>
+                        <div>
+                            <h4 style={{ margin: "2px" }}>Want to have a chat?</h4>
+                            <A className="mustard-text" href="mailto:oielbanna@email.com">oielbanna@gmail.com</A>
+                        </div>
+
+                       
+                        <Links className="links"/>
+
+                    </div>
+                </div>
+                <div className="graphic_area" >
                     <LanguageSelector />
                     <img src={require("../../assets/nav-art.png")} height="600" />
                 </div>
