0fa24329c554658ea5112fde34c25d4d13a9dea2
Omar Ibrahim <oielbanna@gmail.com>
2020-11-19 00:22:32 -0500

    single project component

--- src/views/Projects.js
+++ src/views/Projects.js
@@ -3,9 +3,12 @@ import { projects } from '../constants';
 import SingleProject from "./components/SingleProject";
 import '../styles/projects.scss';
 
+// This one is for staggering children
+// https://codesandbox.io/s/framer-motion-side-menu-mx2rw?fontsize=14&module=/src/Example.tsx&file=/src/Navigation.tsx:112-290
 export default () => {
 	return (
 		<section id="projects">
+			{/* <h1>Projects I'm proud of</h1> */}
 			<ul>
 				{projects.map((project, i) => {
 					return <SingleProject id={i + 1} {...project} />
--- src/views/components/SingleProject.js
+++ src/views/components/SingleProject.js
@@ -1,11 +1,15 @@
 import React from "react"
-import { motion } from 'framer-motion';
+import { motion, useViewportScroll, useTransform } from "framer-motion";
 import '../../styles/projects.scss';
 import { A } from '.';
-
+// the following tutorial is for making the opacity animation
+// https://medium.com/better-programming/framer-motion-tutorials-make-more-advanced-animations-4344b686ea0a
 export default ({ name, slug, github, description, preview, id }) => {
+	const { scrollY } = useViewportScroll()
+	const opacity = useTransform(scrollY, [0, 150, 300], [0.4, 1, 0.4]);
+
 	return (
-		<li key={id} className="project">
+		<motion.li key={id} className="project" style={{ opacity }}>
 			<A href={github}>
 				<div className="row">
 					<span className="project-order">{id < 9 ? 0 : null}{id}</span>
@@ -13,6 +17,6 @@ export default ({ name, slug, github, description, preview, id }) => {
 					<p className="project-slug">{slug}</p>
 				</div>
 			</A>
-		</li>
+		</motion.li>
 	)
 }
