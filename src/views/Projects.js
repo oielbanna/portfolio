e3113d88b8174a0d1bc23a432d337e7e3d94ef03
Omar Ibrahim <oielbanna@gmail.com>
2020-11-22 23:11:44 -0500

    stagger children

--- src/views/Projects.js
+++ src/views/Projects.js
@@ -1,19 +1,30 @@
 import React from "react"
+import { motion, useViewportScroll, useTransform } from "framer-motion";
 import { projects } from '../constants';
 import SingleProject from "./components/SingleProject";
 import '../styles/projects.scss';
 
 // This one is for staggering children
 // https://codesandbox.io/s/framer-motion-side-menu-mx2rw?fontsize=14&module=/src/Example.tsx&file=/src/Navigation.tsx:112-290
+
+const staggerVariants = {
+  open: {
+    transition: { staggerChildren: 0.07, delayChildren: 0.2 }
+  },
+  closed: {
+    transition: { staggerChildren: 0.05, staggerDirection: -1 }
+  }
+};
+
 export default () => {
 	return (
 		<section id="projects">
 			{/* <h1>Projects I'm proud of</h1> */}
-			<ul>
+			<motion.ul variants={staggerVariants} initial="closed" animate="open">
 				{projects.map((project, i) => {
 					return <SingleProject id={i + 1} {...project} />
 				})}
-			</ul>
+			</motion.ul>
 		</section>
 	)
 }
--- src/views/components/SingleProject.js
+++ src/views/components/SingleProject.js
@@ -4,12 +4,29 @@ import '../../styles/projects.scss';
 import { A } from '.';
 // the following tutorial is for making the opacity animation
 // https://medium.com/better-programming/framer-motion-tutorials-make-more-advanced-animations-4344b686ea0a
+
+const variants = {
+  open: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      y: { stiffness: 1000, velocity: -100 }
+    }
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+    transition: {
+      y: { stiffness: 1000 }
+    }
+  }
+};
 export default ({ name, slug, github, description, preview, id }) => {
 	const { scrollY } = useViewportScroll()
 	const opacity = useTransform(scrollY, [0, 150, 300], [0.4, 1, 0.4]);
 
 	return (
-		<motion.li key={id} className="project" style={{ opacity }}>
+		<motion.li key={id} variants={variants} className="project" style={{ opacity }}>
 			<A href={github}>
 				<div className="row">
 					<span className="project-order">{id < 9 ? 0 : null}{id}</span>
