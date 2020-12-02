3da2b8aaa03bd5dca82ccfae23276e722260b721
Omar Ibrahim <oielbanna@gmail.com>
2020-12-01 23:08:57 -0500

    add accessibility for biolength component, animate bio appear

--- src/constants/index.js
+++ src/constants/index.js
@@ -16,10 +16,10 @@ const LONGEST = 'longest';
 export const BIO_LENGTHS = [SHORTEST, MID, LONGEST];
 
 const SHORTEST_COMPONENT = () => {
-    return <p><strong>Omar</strong> is trying his very best.</p>
+    return <p><strong>Omar</strong> is trying his very best.</p>;
 }
 const MID_COMPONENT = () => {
-    return <p><strong>Omar</strong> likes to make <span className="fun">fun,</span> <span className="shake">interactive</span> things with code. He gets particularly excited when these things involve working with others to make an even bigger impact.</p>
+    return <p><strong>Omar</strong> likes to make <span className="fun">fun,</span> <span className="shake">interactive</span> things with code. He gets particularly excited when these things involve working with others to make an even <span style={{ fontVariant: "small-caps" }}>bigger</span> impact.</p>
 }
 const LONGEST_COMPONENT = () => {
     return <p><strong>Omar</strong> is a full-stack web developer at <A className="link" href="https://www.capitalone.ca/">Capital One</A> based in Toronto, Canada (but that's flexible considering, you know...the pandemic). He likes to <A className="link" target="_blank" href={CONTACTS.GITHUB.URL}>code</A> with React/Vue and Spring Boot. But his true passion lies in Software Architecture. So, he set out on a journey to take the <A className="link" href="https://aws.amazon.com/certification/certified-solutions-architect-associate/">AWS Certified Solutions Architect</A> Course - which you can follow on <A className="link" href={CONTACTS.TWITTER.URL}>Twitter.</A> He also enjoys writing Medium articles sometimes - he says it helps him <q><i>explore a new medium.</i></q><small> (get it?)</small></p>
@@ -48,7 +48,7 @@ export const PROJECTS = [{
     slug: 'Open Source Contribution',
     description: '',
     preview: require('../assets/logo.svg'),
-},{
+}, {
     name: 'React Bird Game',
     github: 'https://github.com/oielbanna/react-plane-game',
     slug: 'How to describe this?',
@@ -75,7 +75,7 @@ export const EXPERIENCE = [{
 }];
 
 export const EDUCATION = {
-    
+
 }
 
 export const STARS_COORDS = [
@@ -526,4 +526,4 @@ export const STARS_COORDS = [
     "M167 49zM167 49c2-2 4-2 6-1h3c1-1 0-2-1-3v-6l1-2 1-1-2-1h-5l-4-4c-2-1-1 1-2 1v5h-1l-5 2c-2 1-2 2 0 3l5 4v1c0 2-1 4 1 4 1 1 2-1 3-2z",
     "M178 199v1l1 4c-1 3 1 4 3 4s3 2 4 3c-1 4-5 3-7 4l-1 4v1c-1 1 0 3-2 3s-2-1-3-2l-1-3c-2-2-3-1-5-1h-4c-1-1 2-2 2-4v-7c-1-1-2-2-1-4l6 2c1 0 3 0 4-2l4-3z",
     "M188 257l5 6c1 1 1 2-1 2-2 1-4 1-4 4h4c1-1 2-2 3 1l-2 5c-1 1-2 3 0 4 1 1 2-1 2-2l1-1 1-3h2v2l4 4v-2c0-2-2-4-1-5l4 2h1l1 1 3-1-2-1-6-4c0-2 1-2 2-2h5c1-1 2-1 0-2h-3c-7-1-7-1-5-8v-3l-1-3c-2 0-2 2-2 3l-2 7h-1l-2-5-1-2-1-2c-1-1-1-3-3-3-1 1 0 2 1 3s2 2 0 3c-1 0-2-2-3 0l1 2z",
-    ];
+];
--- src/views/About.js
+++ src/views/About.js
@@ -1,9 +1,16 @@
 import React, { useEffect, useState } from "react";
-import { motion, useViewportScroll, useTransform } from "framer-motion";
+import { motion, useViewportScroll, useTransform, AnimatePresence } from "framer-motion";
 import "../styles/about.scss";
 import { ABOUT, BIO_LENGTHS } from "../constants";
 
 function BioLength({ bio, changeBio }) {
+  const handleEnter = (item, $e) => {
+    console.log($e);
+    if ($e.key === 'Enter') {
+      changeBio(item)
+    }
+  }
+
   return (
     <form id="bio-length-form">
       <fieldset className="fieldset">
@@ -11,8 +18,8 @@ function BioLength({ bio, changeBio }) {
         <ul className="container">
           {BIO_LENGTHS.map((item, i) => {
             return (
-              <li key={i}>
-                <input id={'input' + (i + 1)} name="bio-length" type="radio" value={item} checked={bio === item} onChange={() => changeBio(item)} />
+              <li tabIndex="0" key={i} onKeyDown={($e) => handleEnter(item, $e)}>
+                <input id={'input' + (i + 1)} name="bio-length" type="radio" value={item} checked={bio === item} onChange={() => changeBio(item)} onKeyDown={($e) => handleEnter(item, $e)} />
                 <label htmlFor={'input' + (i + 1)} >{i === 0 || i === 2 ? item : ""}</label>
               </li>
             );
@@ -43,8 +50,6 @@ export default () => {
   const [bio, changeBio] = useState(BIO_LENGTHS[Math.floor(BIO_LENGTHS.length / 2)]);
   const { scrollY } = useViewportScroll()
   const opacity = useTransform(scrollY, [15, 100], [1, 0]);
-  // const y = useTransform(scrollY, [20, 158], [-190, 2]);
-  // const y = useTransform(scrollY, [0, 40], [-190, -150]);
   const y = useTransform(scrollY, [0, 158], [-230, -120]);
 
   useEffect(() => {
@@ -97,19 +102,28 @@ export default () => {
         >
           Omar Ibrahim.
         </motion.h1>
-        <motion.div initial={initialAnimate}
+        <motion.div 
+          initial={initialAnimate}
           animate={introAnimate}
           transition={defaultTransition}>
           <BioLength
             bio={bio}
             changeBio={changeBio}
-
           />
         </motion.div>
       </div>
       <div className="bio_text-container">
-        {/* TODO: use AnimatePresence here!! */}
-        {ABOUT[bio]}
+        <AnimatePresence exitBeforeEnter>
+          <motion.div
+            key={bio}
+            initial={{ opacity: 0, y: 40 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 40 }}
+            transition={defaultTransition}
+          >
+            {ABOUT[bio]}
+          </motion.div>
+        </AnimatePresence>
       </div>
     </section >
   )
