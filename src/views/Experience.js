b72c15903d7fb1798f688260e06ff5a7b16339b5
Omar Ibrahim <oielbanna@gmail.com>
2020-12-22 00:37:44 -0500

    Update sizes for experience section

--- src/views/Experience.js
+++ src/views/Experience.js
@@ -2,38 +2,8 @@ import React from "react";
 import { motion, useViewportScroll, useTransform, useSpring } from 'framer-motion';
 import { JOURNEY } from "../constants";
 import styled from "styled-components";
+import breakpoint from 'styled-components-breakpoint';
 import { A } from './components';
-
-// const Connection = ({ direction, offset }) => {
-//     const { scrollY } = useViewportScroll();
-//     const yRange = useTransform(scrollY, [(window.innerHeight + offset) - 350, (window.innerHeight + offset)], [0.01, 1]);
-//     const pathLength = useSpring(yRange, { stiffness: 400, damping: 90 });
-
-//     return (
-//         <SVG viewBox="0 0 10 10">
-//             <motion.path
-//                 fill="none"
-//                 strokeWidth="0.1"
-//                 stroke="white"
-//                 d="M 0 1.5 C 4.2 2.6 8.5 5.8 8.9 3.1 C 9.1 1.1 5.1 1.4 6.6 5.6 C 6.8 6.3 7.7 8.1 6 9.9"
-//                 style={{
-//                     pathLength,
-//                     scaleX: direction // Reverse direction of line animation
-//                 }}
-//             />
-//         </SVG>
-//     );
-// }
-
-// Connection.propTypes = {
-//     direction: PropTypes.oneOf([1, -1]).isRequired
-// };
-
-// const SVG = styled.svg({
-//     // maxWidth: '100%',
-//     // maxHeight: 300,
-//     width: 100
-// })
 
 export default () => {
     const { scrollY } = useViewportScroll();
@@ -41,62 +11,96 @@ export default () => {
     const rotate = useSpring(rotationRange, { stiffness: 400, damping: 90 });
 
     return (
-        <section style={{ height: '180vh'}} id="experience" className="experience">
+        <section style={{ height: '180vh', paddingTop: 'unset' }} id="experience" className="experience">
             <Sticky style={{ height: '70vh' }}>
                 <h1 className="section-title">how i got here</h1>
                 <JourneyContainer
                     style={{ rotate }}
                 >
-                    <div style={{
-                        position: 'absolute',
-                        left: '50%',
-                        top: '-95%',
-                        transform: 'translate(-50%, 50%) scale(-1)',
-                        textOrientation: 'sideways-right',
-                        writingMode: 'vertical-rl',
-
-                    }}>
+                    <Education>
                         <Intro>{JOURNEY[0].category}</Intro>
-                        <Title style={{ marginRight: 10}}>{JOURNEY[0].title} <A>@{JOURNEY[0].entity}</A></Title>
+                        <Title style={{ marginRight: 10 }}>{JOURNEY[0].title} <A>@{JOURNEY[0].entity}</A></Title>
                         <Subtitle>{JOURNEY[0].dateRange}</Subtitle>
                         <Details>
-                            {JOURNEY[0].description.map((item, i) => <li key={i}>{item}</li>)}
+                            {JOURNEY[0].description.map((item, i) => <li key={i} style={{ marginLeft: 5 }} className="details-item">{item}</li>)}
                         </Details>
-                    </div>
-                    <div style={{
-                        position: 'absolute',
-                        top: '50%',
-                        right: '-90%',
-                        transform: 'translate(-50%, -50%)',
-                    }}>
+                    </Education>
+                    <Internships>
                         <Intro>{JOURNEY[1].category}</Intro>
                         <Title>{JOURNEY[1].title} <A>@{JOURNEY[1].entity}</A></Title>
                         <Subtitle>{JOURNEY[1].dateRange}</Subtitle>
                         <Details>
-                            {JOURNEY[1].description.map((item, i) => <li key={i}>{item}</li>)}
+                            {JOURNEY[1].description.map((item, i) => <li key={i} style={{ marginBottom: 5 }} className="details-item">{item}</li>)}
                         </Details>
-                    </div>
-                    <div style={{
-                        position: 'absolute',
-                        bottom: '-110%',
-                        left: '50%',
-                        transform: 'translate(-50%, -50%)',
-                        transformOrigin: 'top left',
-                        textOrientation: 'sideways-right',
-                        writingMode: 'vertical-rl',
-                    }}>
+                    </Internships>
+                    <Work>
                         <Intro>{JOURNEY[2].category}</Intro>
                         <Title>{JOURNEY[2].title} <A>@{JOURNEY[2].entity}</A></Title>
                         <Subtitle>{JOURNEY[2].dateRange}</Subtitle>
                         <Details>
-                            {JOURNEY[2].description.map((item, i) => <li key={i}>{item}</li>)}
+                            {JOURNEY[2].description.map((item, i) => <li key={i} style={{ marginLeft: 5 }} className="details-item">{item}</li>)}
                         </Details>
-                    </div>
+                    </Work>
                 </JourneyContainer>
             </Sticky>
-        </section >
+        </section>
     )
 }
+
+const Education = styled.div`
+    position: absolute;
+    text-orientation: sideways-right;
+    writing-mode: vertical-rl;
+    left: 50%;
+    top: -95%;
+    transform: translate(-50%, 50%) scale(-1);
+
+    ${breakpoint('desktop')`
+        max-height: 300px;  
+    `}
+
+    ${breakpoint('mobile')`
+        max-height: 75vw;
+    `}
+`
+
+const Internships = styled.div`
+    position: absolute;
+
+    ${breakpoint('desktop')`
+        max-width: 450px;  
+        top: 50%;
+        right: -100%;
+        transform: translate(0%, -50%);
+    `}
+
+    ${breakpoint('mobile')`
+        max-width: 75vw;
+        top: 50%;
+        right: -90%;
+        transform: translate(-50%, -50%);
+    `}
+`
+const Work = styled.div`
+    position: absolute;
+    text-orientation: sideways-right;
+    writing-mode: vertical-rl;
+    
+
+    ${breakpoint('desktop')`
+        max-height: 450px; 
+        bottom: -100%;
+        left: 50%;
+        transform: translate(-50%, 0%);
+    `}
+
+    ${breakpoint('mobile')`
+        max-height: 75vw;
+        bottom: -96%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    `}
+`
 
 const Sticky = styled.div`
     position: sticky; 
@@ -137,15 +141,11 @@ const Subtitle = styled.h3`
 `
 
 const Details = styled.ul`
-    list-style: none;
-    margin: 0px;
-
-    li::before {
-        content: "\2022";
-        color: #c68742;
-        font-weight: bold; 
-        display: inline-block;
-        width: 1em;
-        margin-left: -2em; 
+    padding-inline-start: 30px;
+    
+    .details-item {
+        &::marker {
+            color: #c68742;
+        }
     }
 `
