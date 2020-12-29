007f9fcee7d1a0f9b2f2a566db901880844ca2d0
Omar Ibrahim <oielbanna@gmail.com>
2020-12-29 16:55:47 -0500

    Fix mobile footer

--- src/views/components/Footer.js
+++ src/views/components/Footer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { motion, useViewportScroll } from 'framer-motion';
 import styled from 'styled-components';
+import breakpoint from 'styled-components-breakpoint';
 import { A } from '.';
 import SocialLinks from './SocialLinks';
 
@@ -19,14 +20,13 @@ const Footer = () => {
 			<br />
 			<A href="mailto:oielbanna@gmail.com">oielbanna@gmail.com</A>
 
-			<SocialLinks isShown={show} isColumn={true}/>
+			<SocialLinks isShown={show} isColumn={true} />
 		</Section>);
 }
 
 export default Footer;
 
 const Section = styled(motion.footer)`
-	padding: 80px 14rem 110px;
 	background-color: var(--accent-dark-color);
 	font-size: 1.3rem;
 
@@ -36,4 +36,13 @@ const Section = styled(motion.footer)`
 		font-size: 2rem;
 		margin-bottom: 20px;
 	}
+
+	${breakpoint('desktop')`
+		padding: 80px 14rem 110px;
+	`}
+	
+	${breakpoint('mobile')`
+		padding: 35px;
+		padding-left: 24px;
+  `}
 `
